3e50d9f8c6827c894c142c27027103c28943162d
jahid <jahidulh829@gmail.com>
2026-04-07 18:53:05 +0600

    feat: implement UploadPage with UI for PDF analysis

--- lib/app/nebula.dart
+++ lib/app/nebula.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:nebula/features/ai_assistant/presentation/pages/upload_page.dart';
 
 class Nebula extends StatelessWidget {
   const Nebula({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: UploadPage(),
+    );
   }
+
 }
--- lib/features/ai_assistant/presentation/pages/upload_page.dart
+++ lib/features/ai_assistant/presentation/pages/upload_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class UploadPage extends StatefulWidget {
   const UploadPage({super.key});
 
@@ -9,6 +10,55 @@ class UploadPage extends StatefulWidget {
 class _UploadPageState extends State<UploadPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 30),
+            Text(
+              "PDF Analyzer",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Upload and analyze your documents with AI",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.cloud_upload, size: 50, color: Colors.grey),
+                    SizedBox(height: 10),
+                    Text(
+                      "Tap to upload PDF",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Supported formats: PDF",
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
